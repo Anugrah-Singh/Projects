6b28e6d9e81d982df94d73540b3b74cfe3902081
Anugrah-Singh <anugrahsinngh@gmail.com>
2024-12-29 16:13:54 +0530

    React

---
+++ 03tailwindProps/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  <>
+    <h1 className='bg-green-400'>Tailwind Test</h1>
+  </>
+}
+
+export default App
