aff2a9d57f28b981622a3880ca080d4469d89c3a
Anugrah-Singh <anugrahsinngh@gmail.com>
2024-12-30 22:29:35 +0530

    Add initial setup for React and Tailwind CSS with Vite

--- 03tailwindProps/src/App.jsx
+++ 03tailwindProps/src/App.jsx
@@ -2,11 +2,24 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
+
 
 function App() {
-  <>
-    <h1 className='bg-green-400'>Tailwind Test</h1>
-  </>
+
+  let myObj = {
+    username: 'Hitesh',
+    age: 21
+  }
+
+  return(
+    <>
+      <h1 className='bg-green-400 text-black rounded-xl mb-4'>Tailwind Test</h1>
+      <Card username = 'chaiAurCode' btnText = 'click me' />
+      <Card username = 'Anugrah' />
+        
+    </>
+  )
 }
 
 export default App
