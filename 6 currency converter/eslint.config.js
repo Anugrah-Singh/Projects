2ee8eb1d985615d4481e94e68a49ae29aabd74b8
Anugrah-Singh <anugrahsinngh@gmail.com>
2025-01-03 20:05:06 +0530

    React Router

--- 6 currency converter/eslint.config.js
+++ 6 currency converter/eslint.config.js
@@ -33,7 +33,33 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
-      'react/prop-types': 'warn', // Change prop-types validation to warning
+      'no-unused-vars': 'warn',
+      'react/prop-types': 'warn',
     },
   },
 ]
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'no-unused-vars': 'warn',
+    'react/prop-types': 'warn',
+  },
+};
